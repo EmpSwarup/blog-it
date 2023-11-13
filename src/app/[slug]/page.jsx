07a79ea5390bae2d40533dfaa684b5dd8a014200
Swarup Sapkota <empswarupcoc@gmail.com>
2023-11-13 18:17:18 +0545

    first commit

---
+++ src/app/[slug]/page.jsx
@@ -0,0 +1,7 @@
+import styles from "./singlePage.module.css";
+
+const page = () => {
+  return <div className={styles.container}>SinglePage</div>;
+};
+
+export default page;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import styles from "./navbar.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import AuthLinks from "../authLinks/AuthLinks";
+import ThemeToggle from "../themeToggle/ThemeToggle";
+
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.social}>
+        <a href="https://github.com/EmpSwarup/blogify" target="_blank">
+          <Image src="/github.png" alt="github" width={24} height={24} />
+        </a>
+        <a href="https://www.linkedin.com/in/swarupsapkota/" target="_blank">
+          <Image src="/linkedin.png" alt="linkedin" width={24} height={24} />
+        </a>
+      </div>
+      <div className={styles.logo}>blogify</div>
+      <div className={styles.links}>
+        <ThemeToggle />
+        <Link href="/" className={styles.link}>
+          Homepage
+        </Link>
+        <Link href="/" className={styles.link}>
+          Contact
+        </Link>
+        <Link href="/" className={styles.link}>
+          About
+        </Link>
+        <AuthLinks />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
